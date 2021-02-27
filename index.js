dcefbfba49846bbb36087e025e7d1dde701bcaf1
alputv <77716705+alputv@users.noreply.github.com>
2021-02-28 00:00:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -286,9 +286,7 @@ client.on('message', async message => {
         avatar: message.author.avatarURL({format: 'png'})
     }).send(newMessage)
     };
-    let denen = message.content.split(' ').find(x => x.startsWith(':') && x.endsWith(':'))
-    if (!denen) return;
-    let emoji = message.content.split(' ').find(x => x.startsWith(':') && x.endsWith(':')).toString().replace(/:/g, '');
+    let emoji = message.content.split(' ').find(x => x.startsWith(':') && x.endsWith(':').toString().replace(/:/g, ''));
     if (emoji) return;
     let emojii = message.guild.emojis.cache.find(x => x.name.includes(emoji));
     if(!emojii) return;
