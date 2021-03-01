e61cce4f355d3fae306bddd06ff2f95f56ed6095
alputv <77716705+alputv@users.noreply.github.com>
2021-03-01 15:02:55 +0300

    Create nitrosuz-emoji-aç.js

---
+++ commands/system/nitrosuz-emoji-aç.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const { sistem } = require('../../database/SistemDataBase')
+module.exports = {
+ name: 'nitrosuz-emoji-aç',
+ aliases: ['nitrosuzemojiaç'] ,
+ description: 'Sunucu için nitrosuz emoji sistemini açarsınız.',
+ usage: ['nitrosuz-emoji-aç'],
+/** 
+* @param {Discord.Client} client
+* @param {Discord.Message} message
+* @param {String[]} args
+*/
+run: async (client , message ,args) => {
+    const noperm = new Discord.MessageEmbed()
+    .setColor('RED')
+    .setDescription(`**${message.author.username}** bu komutu kullanabilmek için \`Sunucuyu Yönet\` yetkisine sahip olmalısın!`)
+    if(!message.guild.me.hasPermission(['SEND_MESSAGES','MANAGE_ROLES','EMBED_LINKS'])) return
+    if(!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send(noperm.setTitle('<:hata:813391295665930260> Yetersiz Yetki!'))
+
+
+
+await sistem.set(`saas_${message.guild.id}` , true)
+message.channel.send(new Discord.MessageEmbed().setDescription(`**Nitrosuz Emoji** sistemi açıldı!`))
+}
+}
