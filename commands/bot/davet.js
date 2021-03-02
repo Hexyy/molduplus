0ebd0a709f74f7af56ae895c2dd2be25155807e9
alputv <77716705+alputv@users.noreply.github.com>
2021-03-02 11:44:33 +0300

    Update davet.js

--- commands/bot/davet.js
+++ commands/bot/davet.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 module.exports = {
  name: 'davet',
- aliases: ['invite', 'bot-davet'] ,
+ aliases: ['invite', 'bot-davet', 'Davet'] ,
  description: 'Botun davet linkini alırsınız.',
  usage: ['davet'],
 /** 
@@ -11,6 +11,6 @@ module.exports = {
 * @param {String[]} args
 */
 run: async (client , message ,args) => {
-message.channel.send(new Discord.MessageEmbed().setAuthor("Moldup'ı sevdiniz mi?", message.author.displayAvatarURL({ dynamic: true })).setDescription("[Sunucunuza davet edin](https://bot.moldup.tk/davet.html)").setFooter(message.author.tag + " tarafından istendi.").setTimestamp().setColor("BLUE"))
+message.channel.send(new Discord.MessageEmbed().setAuthor("Moldup'ı sevdiniz mi?", message.author.displayAvatarURL({ dynamic: true })).setDescription("[Sunucunuza davet edin](https://bot.moldup.tk/davet.html)").setFooter(message.author.tag + " tarafından istendi").setTimestamp().setColor("BLUE"))
 }
 }
