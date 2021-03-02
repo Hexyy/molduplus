c0488067def3f0fc196c9e08d9650e288d83add1
alputv <77716705+alputv@users.noreply.github.com>
2021-03-02 14:12:49 +0300

    Update ayarlar.js

--- settings/ayarlar.js
+++ settings/ayarlar.js
@@ -1,5 +1,5 @@
 const ayarlar = {
-token: 'NzYxNTcxNDQzMTk2NTU5Mzcx.X3cirQ.xOyfAzh_qA430ycLo3ClBne4bn8', //Nzk3ODQyODY2NzgxMDI4Mzcz.X_sXEg.YhmLVcz6zyTPrSkjYulP37A-Uhc
+token: 'NzYxNTcxNDQzMTk2NTU5Mzcx.X3cirQ.gF5LyA8_ITuvpHPlwsiH1ljGZiA',
 prefix: '?',
 mongoDB:'mongodb+srv://moldup:moldup123@moldup.ulpqw.mongodb.net/moldup?retryWrites=true&w=majority'
 }
