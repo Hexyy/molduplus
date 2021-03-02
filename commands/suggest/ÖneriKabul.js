4c2f32fb89173e5df22e15cf1e4a2bd55f9028f9
alputv <77716705+alputv@users.noreply.github.com>
2021-03-02 13:45:38 +0300

    Update ÖneriKabul.js

--- commands/suggest/ÖneriKabul.js
+++ commands/suggest/ÖneriKabul.js
@@ -28,10 +28,10 @@ const channel = message.guild.channels.cache.get(await öneri.fetch(`önerikanal
 if(!channel) return
 const öneriembed = await channel.messages.fetch(mesajID)
 if(!öneriembed) return message.channel.send(`**${mesajID}** ID'li mesaj bulamadım!`)
-const data1 = öneriembed.embeds[0]
-const deskırıpşın = data1.description
+const data = öneriembed.embeds[0]
+const deskırıpşın = data.description
 const embed = new Discord.MessageEmbed()
-.setAuthor(data1.author.tag, "https://cdn.discordapp.com/emojis/798871896636522517.png?v=1")
+.setAuthor(data.author.name, data.avatarURL)
 .setDescription(`${deskırıpşın}`)
 .setColor('GREEN')
 .addField('Durum' , 'Kabul Edildi')
