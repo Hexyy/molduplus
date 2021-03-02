517ddf923256bf56581dd5a64b9cc21bdac71c10
alputv <77716705+alputv@users.noreply.github.com>
2021-03-02 11:48:40 +0300

    Create site.js

---
+++ commands/bot/site.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+
+module.exports = {
+ name: 'site',
+ aliases: ['Site', 'website'] ,
+ description: 'Botun sitesinin linkini alırsınız.',
+ usage: ['site'],
+/** 
+* @param {Discord.Client} client
+* @param {Discord.Message} message
+* @param {String[]} args
+*/
+run: async (client , message ,args) => {
+message.channel.send(new Discord.MessageEmbed().setAuthor("Sitemiz de var!", message.author.displayAvatarURL({ dynamic: true })).setDescription("[Sitemize git](https://bot.moldup.tk)").setFooter(message.author.tag + " tarafından istendi").setTimestamp().setColor("BLUE"))
+}
+}
