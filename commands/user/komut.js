871aa2debd4705566f96c27c11ca614ca6651230
alputv <77716705+alputv@users.noreply.github.com>
2021-03-02 12:01:35 +0300

    Update komut.js

--- commands/user/komut.js
+++ commands/user/komut.js
@@ -16,6 +16,6 @@ if(!args[0]) return message.channel.send(`Bir komut adı girmeyi unuttun!`)
 if(!cmd) return message.channel.send(`Komutlarımda **${args[0]}** adında komut bulamadım!`) 
 
 message.channel.send(`
-\`\`\`fix\n< m!${cmd.usage} >\`\`\`\`\`\`diff\n- Açıklama\n${cmd.description}\n\n- Diğer Kullanımlar\n${cmd.aliases.join(' , ')}\`\`\`\`\`\`md\n> [] = Gerekli argüman\n> {} = İsteğe bağlı argüman\`\`\``)
+\`\`\`fix\n< ?${cmd.usage} >\`\`\`\`\`\`diff\n- Açıklama\n${cmd.description}\n\n- Diğer Kullanımlar\n${cmd.aliases.join(' , ')}\`\`\`\`\`\`md\n> [] = Gerekli argüman\n> {} = İsteğe bağlı argüman\`\`\``)
 }
 }
