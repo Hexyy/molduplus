b470846d2ca22f1cb13195e0b4e109650e703a50
alputv <77716705+alputv@users.noreply.github.com>
2021-03-03 00:46:19 +0300

    Create emoji-sil.js

--- commands/emoji/emoji-sil.js
+++ commands/emoji/emoji-sil.js
@@ -1,10 +1,10 @@
 const Discord = require('discord.js');
 
 module.exports = {
- name: 'emoji-sil',
- aliases: ['emojisil'] ,
-description: 'Sunucu için hızlı yoldan emoji silersiniz.',
-usage: ['emoji-sil [ emoji ]'],
+ name: 'emoji-ekle',
+ aliases: ['emojiekle', 'emoji-yükle', 'emojiyükle'] ,
+description: 'Sunucu için hızlı yoldan emoji eklersiniz.',
+usage: ['emoji-ekle [ emoji ]'],
 /** 
 * @param {Discord.Client} client
 * @param {Discord.Message} message
@@ -16,23 +16,23 @@ run: async (client , message ,args) => {
     .setDescription(`**${message.author.username}** bu komutu kullanabilmek için \`Emojileri Yönet\` yetkisine sahip olmalısın!`)
     if(!message.guild.me.hasPermission(['SEND_MESSAGES','MANAGE_EMOJIS','EMBED_LINKS'])) return
     if(!message.member.hasPermission('MANAGE_EMOJIS')) return message.channel.send(noperm.setTitle('<:hata:813391295665930260> Yetersiz Yetki!'))
-  if(!args[0]) return message.channel.send(`**${message.author.username}** bir emoji adı girmeyi unuttun!`)
+  if(!args[0]) return message.channel.send(new Discord.MessageEmbed().setTitle("<:hata:813391295665930260> Hata!").setColor("RED").setDescription(`**${message.author.username}** bir emoji adı girmeyi unuttun!`))
 try {
 const s = args.slice(0).join(' ').split(' ').filter(x => x.includes('<') && x.includes('>'))[0];
 var emoji;
 if(s) {
-if(!message.guild.emojis.cache.get(s.split(':')[2].split('>')[0])) return message.channel.send(`Yazdığın emojiyi bulamadım!`);
+if(!message.guild.emojis.cache.get(s.split(':')[2].split('>')[0])) return message.channel.send(new Discord.MessageEmbed().setTitle("<:hata:813391295665930260> Hata!").setColor("RED").setDescription(`Yazdığın emojiyi bulamadım veya bu sunucuda bulunmuyor!`));
 emoji = message.guild.emojis.cache.get(s.split(':')[2].split('>')[0]);
 } else {
-if(!message.guild.emojis.cache.find(x => x.name === args.slice(0).join('-'))) return message.channel.send(`Yazdığın emojiyi bulamadım!`);
+if(!message.guild.emojis.cache.find(x => x.name === args.slice(0).join('-'))) return message.channel.send(new Discord.MessageEmbed().setTitle("<:hata:813391295665930260> Hata!").setColor("RED").setDescription(`Yazdığın emojiyi bulamadım veya bu sunucuda bulunmuyor!`));
 emoji = message.guild.emojis.cache.find(x => x.name === args.slice(0).join('-'));
 };
 
 emoji.delete();
-return message.channel.send(`\`${emoji.name}\` isimli emoji silindi!`);
+return message.channel.send(new Discord.MessageEmbed().setTitle(`Görev Tamamlandı!`).setColor("#22BF41").setDescription(`<:moldup_evet:783582088346468384> \`${emoji.name}\` isimli emoji silindi!`));
 
 } catch(error) {
-return message.channel.send(`Yazdığınız emojiyi bulamadım!`);
+return message.channel.sen(new Discord.MessageEmbed().setTitle("<:hata:813391295665930260> Hata!").setColor("RED").setDescription(`Yazdığın emojiyi bulamadım veya bu sunucuda bulunmuyor!`));
 };
   
   
