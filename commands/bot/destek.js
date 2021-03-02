b30cd9d642bc71c752aaad75c4d8711dfdff6a7a
alputv <77716705+alputv@users.noreply.github.com>
2021-03-02 11:41:36 +0300

    Update destek.js

--- commands/bot/destek.js
+++ commands/bot/destek.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 
 module.exports = {
- name: 'davet',
- aliases: ['invite', 'bot-davet'] ,
+ name: 'destek',
+ aliases: ['dc', 'discord', 'support'] ,
  description: 'Botun davet linkini alırsınız.',
  usage: ['davet'],
 /** 
@@ -11,6 +11,6 @@ module.exports = {
 * @param {String[]} args
 */
 run: async (client , message ,args) => {
-message.channel.send(new Discord.MessageEmbed().setAuthor("Yardım mı lazım?", message.author.displayAvatarURL({ dynamic: true })).setDescription("[Destek sunucumuza katılın](https://bot.moldup.tk/destek.html)").setFooter(message.author.tag + " tarafından istendi.").setTimestamp().setColor("BLUE"))
+message.channel.send(new Discord.MessageEmbed().setAuthor("Yardım mı lazım?", message.author.displayAvatarURL({ dynamic: true })).setDescription("[Destek sunucumuza katılın](https://bot.moldup.tk/destek.html)").setFooter(message.author.tag + " tarafından istendi").setTimestamp().setColor("BLUE"))
 }
 }
