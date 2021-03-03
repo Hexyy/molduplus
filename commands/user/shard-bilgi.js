c669c7bdeb156dc66b6fd7e8b68686a20bc5bde8
alputv <77716705+alputv@users.noreply.github.com>
2021-03-03 14:59:42 +0300

    Update shard-bilgi.js

--- commands/user/shard-bilgi.js
+++ commands/user/shard-bilgi.js
@@ -19,8 +19,8 @@ require('moment-duration-format');
 return message.channel.send(new Discord.MessageEmbed()
 .setColor('#eb5c0e')
 .setAuthor('Shard Bilgileri', client.user.avatarURL())
-.setDescription('» Bu sunucu toplam **0** adet shard arasından, **0** numaralı shard üzerinde bulunuyor.')
-.addField('Shard 0', `${client.guilds.cache.size} servers,
+.setDescription('Bu sunucu **' + client.shard.count + ' **adet shard arasından, **' + message.guild.shardID + '** numaralı shard üzerinde bulunuyor.')
+.addField('Shard 0', `${client.guilds.cache.size} sunucu,
 ${moment.duration(client.uptime).format(`w [hafta] d [gün] h [saat] m [dakika] s [saniye]`)}
 ${client.ws.ping}ms!`)
 .setFooter(`${message.author.tag} tarafından istendi`, message.author.avatarURL({ dynamic: true })).setTimestamp());
