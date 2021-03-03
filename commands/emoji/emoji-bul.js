12ab8b9fe86550d3bff17a8a0f6b1e4fa3f68bab
alputv <77716705+alputv@users.noreply.github.com>
2021-03-03 13:36:35 +0300

    Update emoji-bul.js

--- commands/emoji/emoji-bul.js
+++ commands/emoji/emoji-bul.js
@@ -18,7 +18,7 @@ run: async (client , message ,args) => {
 **${message.author.username}** bir emoji adı unuttun!
     `))
 
-if(!client.emojis.cache.find(x => x.name === args[0]) && client.emojis.cache.find(x => x.name.endsWith(args[0])) && !client.emojis.cache.find(x => x.name.startsWith(args[0]))) return message.channel.sendnew Discord.MessageEmbed().setTitle("<a:not_found:767708663141105664> Uyarı").setColor("BLUE").setDescription('Aradığınız isme tam olarak uyan veya benzeyen bir emoji bulamadım.'));
+if(!client.emojis.cache.find(x => x.name === args[0]) && client.emojis.cache.find(x => x.name.endsWith(args[0])) && !client.emojis.cache.find(x => x.name.startsWith(args[0]))) return message.channel.send(new Discord.MessageEmbed().setTitle("<a:not_found:767708663141105664> Uyarı").setColor("BLUE").setDescription('Aradığınız isme tam olarak uyan veya benzeyen bir emoji bulamadım.'));
 if(client.emojis.cache.find(x => x.name.startsWith(args[0])) && !client.emojis.cache.find(x => x.name === args[0])) {
    message.channel.send(new Discord.MessageEmbed().setTitle("<a:not_found:767708663141105664> Uyarı").setColor("BLUE").setDescription(`Aradığınız isme sahip bir emoji bulamadım, onun yerine benzer sonuçlar buldum.`));
    return message.channel.send(client.emojis.cache.filter(x => x.name.startsWith(args[0])).map(a => client.emojis.cache.get(a.id)).slice(0, 30).join(' '));
