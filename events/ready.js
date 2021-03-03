35e9689fce2a70fed2ff0976e3d4f933d3fe1514
Hexyy <72667553+Hexyy@users.noreply.github.com>
2021-03-03 17:57:18 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -5,16 +5,7 @@ const { Client } = require('discord.js')
  */
 module.exports = (client) => {
 const durumlar = ['bot.moldup.tk | ?yardım', 'youtube.com/AlpuTV | Yapımcımın kanalına abone olursan sevinirim 😊']
-setInterval(function()  {
-    const durum = Math.floor(Math.random() * durumlar.length)
-
-    client.user.setPresence( { 
-        activity: {
-    name: "bot.moldup.tk | ?yardım",
-    type: 'WATCHING'
-        }, status: 'online'
-    })    
-}, 1000);
+client.user.setActivity("bot.moldup.tk | ?yardım")
     
 console.log(require('chalk').blue(`${client.user.tag} ismiyle giriş yaptım!`));
 }
