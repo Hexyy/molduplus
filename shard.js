cf08cb947dec0886b32fd737b778a8d5c0ce1727
alputv <77716705+alputv@users.noreply.github.com>
2021-03-04 11:03:46 +0300

    Update shard.js

--- shard.js
+++ shard.js
@@ -13,3 +13,28 @@ shard.on('shardCreate' , shard => {
   const hook = new WebhookClient("811179831051288596", "xZ-IVtqIuAB172gYZgPnax-3_LUGKsHh5oG0DC_z_VSEQ8ny-mDaD4-_GnCa83tVH27u")
   hook.send(`<:cevrimici:758661635619029043> \`${shard.id}\` numaralı shard aktif.`)
 })
+
+shard.on('shardDisconnect' , shard => {
+  const hook = new WebhookClient("811179831051288596", "xZ-IVtqIuAB172gYZgPnax-3_LUGKsHh5oG0DC_z_VSEQ8ny-mDaD4-_GnCa83tVH27u")
+  hook.send(`<:cevrimdisi:758661564055945226> \`${shard.id}\` numaralı shardın bağlantısı koptu.`)
+})
+
+shard.on('shardError' , shard => {
+  const hook = new WebhookClient("811179831051288596", "xZ-IVtqIuAB172gYZgPnax-3_LUGKsHh5oG0DC_z_VSEQ8ny-mDaD4-_GnCa83tVH27u")
+  hook.send(`<:rahatsizetmeyin:758661755634581524> \`${shard.id}\` numaralı shardda bir hata var.`)
+})
+
+shard.on('shardReady' , shard => {
+  const hook = new WebhookClient("811179831051288596", "xZ-IVtqIuAB172gYZgPnax-3_LUGKsHh5oG0DC_z_VSEQ8ny-mDaD4-_GnCa83tVH27u")
+  hook.send(`<:cevrimici:758661635619029043> \`${shard.id}\` numaralı shard hazır.`)
+})
+
+shard.on('shardReconnecting' , shard => {
+  const hook = new WebhookClient("811179831051288596", "xZ-IVtqIuAB172gYZgPnax-3_LUGKsHh5oG0DC_z_VSEQ8ny-mDaD4-_GnCa83tVH27u")
+  hook.send(`<:cevrimici:758661635619029043> \`${shard.id}\` numaralı shard tekrar bağlanıyor.`)
+})
+
+shard.on('shardResume' , shard => {
+  const hook = new WebhookClient("811179831051288596", "xZ-IVtqIuAB172gYZgPnax-3_LUGKsHh5oG0DC_z_VSEQ8ny-mDaD4-_GnCa83tVH27u")
+  hook.send(`<:cevrimici:758661635619029043> \`${shard.id}\` numaralı shard aktif olmaya devam ediyor.`)
+})
