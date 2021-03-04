af500828336041bedb222480cc113445e7133561
alputv <77716705+alputv@users.noreply.github.com>
2021-03-04 11:11:36 +0300

    Update shard-bilgi.js

--- commands/user/shard-bilgi.js
+++ commands/user/shard-bilgi.js
@@ -22,7 +22,7 @@ return message.channel.send(new Discord.MessageEmbed()
 .setDescription('Bu sunucu **' + client.shard.count + ' **adet shard arasından, **' + message.guild.shardID + '** numaralı shard üzerinde bulunuyor.')
 .addField('Shard ' + message.guild.shardID, `**Sunucu sayısı:** ${client.guilds.cache.size}
 **Uptime:** ${moment.duration(client.uptime).format(`w [hafta] d [gün] h [saat] m [dakika] s [saniye]`)}
-**Gecikme süresi:** ${client.ws.ping}ms!`)
+**Gecikme süresi:** ${client.ws.ping} ms`)
 .setFooter(`${message.author.tag} tarafından istendi`, message.author.avatarURL({ dynamic: true })).setTimestamp());
 
 }
