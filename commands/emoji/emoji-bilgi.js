c885de97062be8cf1120719be5dbc14e47f33a9a
Hexyy <72667553+Hexyy@users.noreply.github.com>
2021-03-04 14:15:22 +0300

    Update emoji-bilgi.js

--- commands/emoji/emoji-bilgi.js
+++ commands/emoji/emoji-bilgi.js
@@ -15,8 +15,8 @@ usage: ['emoji-bilgi [ emoji ismi ]'],
 run: async (client , message ,args) => {
       if(!message.guild.me.hasPermission(['SEND_MESSAGES','MANAGE_EMOJIS','EMBED_LINKS'])) return
   if(!args[0]) return message.channel.send(new Discord.MessageEmbed().setTitle("<:hata:813391295665930260> Hata!").setColor("RED").setDescription(`**${message.author.username}** bir emoji adı girmeyi unuttun!`))
-const s = args.slice(0).join(' ').split(' ').filter(x => x.includes('<') && x.includes('>'))[0];
-  var emoji;
+const s = args.slice(0).join(' ').split(' ').filter(x => x.includes('<') && x.includes('>'))[0]
+  var emoji
   if(s) {
   if(!message.guild.emojis.cache.get(s.split(':')[2].split('>')[0])) return message.channel.send(new Discord.MessageEmbed().setTitle("<:hata:813391295665930260> Hata!").setColor("RED").setDescription(`Yazdığın emojiyi bulamadım veya bu sunucuda bulunmuyor!`));
   emoji = message.guild.emojis.cache.get(s.split(':')[2].split('>')[0]);
