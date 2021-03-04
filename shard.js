efb1e97ec34bf54eb4efb21f9b6f9767b927d3dd
alputv <77716705+alputv@users.noreply.github.com>
2021-03-04 11:53:22 +0300

    Update shard.js

--- shard.js
+++ shard.js
@@ -10,33 +10,45 @@ shard.spawn()
 
 
 shard.on('shardCreate' , shard => {
+  const { ShardingManager, Client, WebhookClient } = require('discord.js');
+  const client = new Client();
   const hook = new WebhookClient("811179831051288596", "xZ-IVtqIuAB172gYZgPnax-3_LUGKsHh5oG0DC_z_VSEQ8ny-mDaD4-_GnCa83tVH27u")
   hook.send(`<:cevrimici:758661635619029043> \`${shard.id+1}\` numaralı shard aktif.`)
-  client.user.setActivity(`youtube.com/AlpuTV [${shard.id+1}/${client.shard.count}]`, {"type": "WATCHING"});
+  client.user.setActivity(`youtube.com/AlpuTV [${shard.id+1}/${client.shard.count}]`, { type: "WATCHING" });
 })
 
 shard.on('shardDisconnect' , shard => {
+  const { ShardingManager, Client, WebhookClient } = require('discord.js');
+  const client = new Client();
   const hook = new WebhookClient("811179831051288596", "xZ-IVtqIuAB172gYZgPnax-3_LUGKsHh5oG0DC_z_VSEQ8ny-mDaD4-_GnCa83tVH27u")
   hook.send(`<:cevrimdisi:758661564055945226> \`${shard.id+1}\` numaralı shardın bağlantısı koptu.`)
 })
 
 shard.on('shardError' , shard => {
+  const { ShardingManager, Client, WebhookClient } = require('discord.js');
+  const client = new Client();
   const hook = new WebhookClient("811179831051288596", "xZ-IVtqIuAB172gYZgPnax-3_LUGKsHh5oG0DC_z_VSEQ8ny-mDaD4-_GnCa83tVH27u")
   hook.send(`<:rahatsizetmeyin:758661755634581524> \`${shard.id+1}\` numaralı shardda bir hata var.`)
 })
 
 shard.on('shardReady' , shard => {
+  const { ShardingManager, Client, WebhookClient } = require('discord.js');
+  const client = new Client();
   const hook = new WebhookClient("811179831051288596", "xZ-IVtqIuAB172gYZgPnax-3_LUGKsHh5oG0DC_z_VSEQ8ny-mDaD4-_GnCa83tVH27u")
   hook.send(`<:cevrimici:758661635619029043> \`${shard.id+1}\` numaralı shard hazır.`)
 })
 
 shard.on('shardReconnecting' , shard => {
+  const { ShardingManager, Client, WebhookClient } = require('discord.js');
+  const client = new Client();
   const hook = new WebhookClient("811179831051288596", "xZ-IVtqIuAB172gYZgPnax-3_LUGKsHh5oG0DC_z_VSEQ8ny-mDaD4-_GnCa83tVH27u")
-  hook.send(`<:cevrimici:758661635619029043> \`${shard.id+1}\` numaralı shard tekrar bağlanıyor.`)
+  client.user.setActivity(`youtube.com/AlpuTV [${shard.id+1}/${client.shard.count}]`, { type: "WATCHING" });
 })
 
 shard.on('shardResume' , shard => {
+  const { ShardingManager, Client, WebhookClient } = require('discord.js');
+  const client = new Client();
   const hook = new WebhookClient("811179831051288596", "xZ-IVtqIuAB172gYZgPnax-3_LUGKsHh5oG0DC_z_VSEQ8ny-mDaD4-_GnCa83tVH27u")
   hook.send(`<:cevrimici:758661635619029043> \`${shard.id+1}\` numaralı shard aktif olmaya devam ediyor.`)
-  client.user.setActivity(`youtube.com/AlpuTV [${shard.id+1}/${client.shard.count}]`, {"type": "WATCHING"});
+  client.user.setActivity(`youtube.com/AlpuTV [${shard.id+1}/${client.shard.count}]`, { type: "WATCHING" });
 })
