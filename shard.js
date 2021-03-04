c548bcbcc18faa712fa7547c161fbbf4eb6bee9b
alputv <77716705+alputv@users.noreply.github.com>
2021-03-04 11:21:27 +0300

    Update shard.js

--- shard.js
+++ shard.js
@@ -11,30 +11,30 @@ shard.spawn()
 
 shard.on('shardCreate' , shard => {
   const hook = new WebhookClient("811179831051288596", "xZ-IVtqIuAB172gYZgPnax-3_LUGKsHh5oG0DC_z_VSEQ8ny-mDaD4-_GnCa83tVH27u")
-  hook.send(`<:cevrimici:758661635619029043> \`${shard.id}\` numaralı shard aktif.`)
+  hook.send(`<:cevrimici:758661635619029043> \`${shard.id+1}\` numaralı shard aktif.`)
 })
 
 shard.on('shardDisconnect' , shard => {
   const hook = new WebhookClient("811179831051288596", "xZ-IVtqIuAB172gYZgPnax-3_LUGKsHh5oG0DC_z_VSEQ8ny-mDaD4-_GnCa83tVH27u")
-  hook.send(`<:cevrimdisi:758661564055945226> \`${shard.id}\` numaralı shardın bağlantısı koptu.`)
+  hook.send(`<:cevrimdisi:758661564055945226> \`${shard.id+1}\` numaralı shardın bağlantısı koptu.`)
 })
 
 shard.on('shardError' , shard => {
   const hook = new WebhookClient("811179831051288596", "xZ-IVtqIuAB172gYZgPnax-3_LUGKsHh5oG0DC_z_VSEQ8ny-mDaD4-_GnCa83tVH27u")
-  hook.send(`<:rahatsizetmeyin:758661755634581524> \`${shard.id}\` numaralı shardda bir hata var.`)
+  hook.send(`<:rahatsizetmeyin:758661755634581524> \`${shard.id+1}\` numaralı shardda bir hata var.`)
 })
 
 shard.on('shardReady' , shard => {
   const hook = new WebhookClient("811179831051288596", "xZ-IVtqIuAB172gYZgPnax-3_LUGKsHh5oG0DC_z_VSEQ8ny-mDaD4-_GnCa83tVH27u")
-  hook.send(`<:cevrimici:758661635619029043> \`${shard.id}\` numaralı shard hazır.`)
+  hook.send(`<:cevrimici:758661635619029043> \`${shard.id+1}\` numaralı shard hazır.`)
 })
 
 shard.on('shardReconnecting' , shard => {
   const hook = new WebhookClient("811179831051288596", "xZ-IVtqIuAB172gYZgPnax-3_LUGKsHh5oG0DC_z_VSEQ8ny-mDaD4-_GnCa83tVH27u")
-  hook.send(`<:cevrimici:758661635619029043> \`${shard.id}\` numaralı shard tekrar bağlanıyor.`)
+  hook.send(`<:cevrimici:758661635619029043> \`${shard.id+1}\` numaralı shard tekrar bağlanıyor.`)
 })
 
 shard.on('shardResume' , shard => {
   const hook = new WebhookClient("811179831051288596", "xZ-IVtqIuAB172gYZgPnax-3_LUGKsHh5oG0DC_z_VSEQ8ny-mDaD4-_GnCa83tVH27u")
-  hook.send(`<:cevrimici:758661635619029043> \`${shard.id}\` numaralı shard aktif olmaya devam ediyor.`)
+  hook.send(`<:cevrimici:758661635619029043> \`${shard.id+1}\` numaralı shard aktif olmaya devam ediyor.`)
 })
