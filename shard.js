6af78f66c8da8180938c5a13101b893cf06bafb5
Hexyy <72667553+Hexyy@users.noreply.github.com>
2021-03-04 10:55:05 +0300

    Update shard.js

--- shard.js
+++ shard.js
@@ -10,6 +10,6 @@ shard.spawn()
 
 
 shard.on('shardCreate' , shard => {
-  const hook = WebhookClient("811179831051288596", "xZ-IVtqIuAB172gYZgPnax-3_LUGKsHh5oG0DC_z_VSEQ8ny-mDaD4-_GnCa83tVH27u")
+  const hook = new WebhookClient("811179831051288596", "xZ-IVtqIuAB172gYZgPnax-3_LUGKsHh5oG0DC_z_VSEQ8ny-mDaD4-_GnCa83tVH27u")
   hook.send(`<:cevrimici:758661635619029043> \`${shard.id}\` numaralı shard aktif.`)
 })
