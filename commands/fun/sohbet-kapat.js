1f4b287960317959cba0e2e4514e9b2a162c332f
Hexyy <72667553+Hexyy@users.noreply.github.com>
2021-03-06 01:23:12 +0300

    Update sohbet-kapat.js

--- commands/fun/sohbet-kapat.js
+++ commands/fun/sohbet-kapat.js
@@ -12,8 +12,9 @@ module.exports = {
     * @param {String[]} args
     */
     run: async (client, message, args) => {
-if(!await sistem.fetch(`sohbet.${message.author.id}.${message.channel.id}`)) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setTitle('Hata').setDescription(`<:moldup_hayir:783582180113907742> Sohbet zaten kapalı!`))
-        await sistem.delete(`sohbet.${message.author.id}.${message.channel.id}`)
+if(!await sistem.fetch(`sohbet.
+_${message.author.id}_${message.channel.id}`)) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setTitle('Hata').setDescription(`<:moldup_hayir:783582180113907742> Sohbet zaten kapalı!`))
+        await sistem.delete(`sohbet_${message.author.id}_${message.channel.id}`)
     message.channel.send(new Discord.MessageEmbed().setColor("GREEN").setTitle(`Görev Tamamlandı!`).setDescription(`<:moldup_evet:783582088346468384> Bot ile sohbetiniz burada sona erdi.`)).then(a => a.delete({timeout: 10000}));
       
     }
