589d0f7b0a22c4a571b933093b9bd7bc05bbf188
alputv <77716705+alputv@users.noreply.github.com>
2021-03-06 01:23:45 +0300

    Update sohbet.js

--- commands/fun/sohbet.js
+++ commands/fun/sohbet.js
@@ -13,7 +13,7 @@ module.exports = {
     */
     run: async (client, message, args) => {
         if (!message.guild.me.hasPermission(['SEND_MESSAGES'])) return
-await sistem.set(`sohbet.${message.author.id}.${message.channel.id}`, true)
+await sistem.set(`sohbet_${message.author.id}_${message.channel.id}`, true)
     message.channel.send(new Discord.MessageEmbed().setColor("GREEN").setTitle(`<:moldup_evet:783582088346468384> ${message.author.username}, sohbet aktif edildi.`).setDescription(`<:news:767708052668416010> Her mesaj family friendly (aile dostu) olmayabilir. Bot size saçma sapan sözler söyleyebilir.\n<:news:767708052668416010> Bot sizin söylediğiniz şeye beklediğiniz yanıtı vermeyebilir.`).setFooter(`Sohbeti bitirmek için ?sohbet-kapat yazmanız yeterlidir.`))
 
     }
