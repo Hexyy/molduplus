69b333c305225d908e3c9477187873fe8aa25a68
alputv <77716705+alputv@users.noreply.github.com>
2021-03-06 01:01:55 +0300

    Create sohbet.js

---
+++ commands/fun/sohbet.js
@@ -0,0 +1,20 @@
+const { sistem } = require('./database/SistemDataBase')
+
+const Discord = require('discord.js');
+module.exports = {
+    name: 'chat',
+    aliases: ["botlasohbet", "sohbet-et", "sohbetet", "sohbet"],
+    description: 'Bot ile sohbet etmeye başlarsınız.',
+    usage: ['chat'],
+    /** 
+    * @param {Discord.Client} client
+    * @param {Discord.Message} message
+    * @param {String[]} args
+    */
+    run: async (client, message, args) => {
+        if (!message.guild.me.hasPermission(['SEND_MESSAGES'])) return
+await sistem.set(`sohbet.${message.author.id}.${message.channel.id}`, true)
+    message.channel.send(new Discord.MessageEmbed().setColor("BLUE").setTitle(`<:moldup_evet:783582088346468384> ${message.author.username}, sohbet aktif edildi.`).setDescription(`<:news:767708052668416010> Her mesaj family friendly (aile dostu) olmayabilir. Bot size saçma sapan sözler söyleyebilir.\n<:news:767708052668416010> Bot sizin söylediğiniz şeye beklediğiniz yanıtı vermeyebilir.`).setFooter(`Sohbeti bitirmek için ?sohbet-kapat yazmanız yeterlidir.`))
+
+    }
+}
