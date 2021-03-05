6642c932fedd530ec82ee90d5e79204f2e1f75d2
alputv <77716705+alputv@users.noreply.github.com>
2021-03-06 00:45:01 +0300

    Update isim.js

--- commands/kayıt/isim.js
+++ commands/kayıt/isim.js
@@ -25,7 +25,7 @@ const datas = await kayit.fetch(`tag.${message.guild.id}`);
   
   function makeid(length) {
    var result           = '';
-   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   var characters       = 'ABCÇDEFGHIİJKLMNOÖPQRSŞTUÜVWXYZabcçdefghijklmnoöpqrsştuüvwxyz0123456789';
    var charactersLength = characters.length;
    for ( var i = 0; i < length; i++ ) {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
