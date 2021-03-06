2a267592d154e55526020c35f16e420eb79f3b0a
alputv <77716705+alputv@users.noreply.github.com>
2021-03-06 13:12:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ if(!message.guild.me.hasPermission('EMBED_LINKS')) return
 if(kontrol) {
 if(message.content.toLowerCase() === 'sa' || message.content.toLowerCase() === 'selam' || message.content.toLowerCase() === 'sea' ||message.content.toLowerCase() === 'selamun aleyküm' || message.content.toLowerCase() === 'slm') 
 {
-message.channel.send(new Discord.MessageEmbed().setColor(message.member.displayHexColor).setAuthor(`${message.author.tag} aleyküm selam , hoş geldin` , message.author.avatarURL({dynamic:false})))
+message.channel.send(new Discord.MessageEmbed().setColor(message.member.displayHexColor).setAuthor(`${message.author.tag} aleyküm selam , hoş geldin` , message.author.avatarURL({dynamic:true})))
 }}})
 client.on('messageDelete' , async (mesaj) => {
 if(mesaj.author.bot || !mesaj.guild ) return
