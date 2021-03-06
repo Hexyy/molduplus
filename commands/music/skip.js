20f55c35275fce68ca7979ab7d55765fa491582d
alputv <77716705+alputv@users.noreply.github.com>
2021-03-07 01:26:36 +0300

    Update skip.js

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -14,7 +14,6 @@ run: async (client , message ,args) => {
 if(!message.guild.me.hasPermission('SPEAK') && !message.guild.me.hasPermission('CONNECT')) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setTitle('<:hata:813391295665930260> Hata').setDescription(`O sesli kanalda konuşamıyorum, izinlerimi kontrol edin!`)) 
 if(!message.member.voice.channel) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setTitle('<:hata:813391295665930260> Hata').setDescription(`Bir sesli kanalda olmadan bu komutu kullanamazsın!`))  
 if(message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setTitle('<:hata:813391295665930260> Hata').setDescription(`Aynı sesli kanalda değiliz, biraz empati kur!`))  
-if(!args[0]) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setTitle('<:hata:813391295665930260> Hata').setDescription(`Lütfen bir şarkı adı yazınız!`).setTitle('Hata'))
 if(!client.player.isPlaying(message)) {
   return message.channel.send(new Discord.MessageEmbed().setColor('RED').setTitle('<:hata:813391295665930260> Hata').setDescription(`Bu sunucuda herhangi bir şarkı çalmıyor!`))
 } else {
