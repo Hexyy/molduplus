13b8ab0a61282e87fd06e5fc331b005568247ecf
alputv <77716705+alputv@users.noreply.github.com>
2021-03-07 13:19:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -44,6 +44,8 @@ player.on('trackStart', (message, track) => message.channel.send(new Discord.Mes
 
 player.on('trackAdd', (message, queue, track) => message.channel.send(new Discord.MessageEmbed().setColor('BLUE').setTitle(track.title).setURL(track.url).setDescription(`<:moldup_evet:783582088346468384> **${track.title}** isimli şarkı kuyruğa eklendi! [${message.author}]`)))
 player.on('queueEnd' , (message , queue) => message.channel.send(new Discord.MessageEmbed().setColor('BLUE').setDescription(`<:moldup_evet:783582088346468384> Kuyruktaki bütün şarkılar oynatıldığı için sesli kanaldan ayrılıyorum!`)))
+
+client.player = player
 /**
  * --------------------------- Karşılama -------------------
  */
