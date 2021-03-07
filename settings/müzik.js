a7de3232e800e391ecd47514ce0b1938e3fe50a6
Hexyy <72667553+Hexyy@users.noreply.github.com>
2021-03-07 13:27:53 +0300

    Update müzik.js

--- settings/müzik.js
+++ settings/müzik.js
@@ -3,5 +3,11 @@ const { Client } = require('discord.js')
 const player = new Player(new Client() , {
 autoSelfDeaf: true,
 })
+player.on('trackStart', (message, track) => message.channel.send(new Discord.MessageEmbed()
+.setColor('GREEN').setTitle(track.title).setURL(track.url).setDescription(`<:moldup_evet:783582088346468384> **${track.title}** isimli şarkı oynatılıyor! [${message.author}]`)))
+
+player.on('trackAdd', (message, queue, track) => message.channel.send(new Discord.MessageEmbed().setColor('BLUE').setTitle(track.title).setURL(track.url).setDescription(`<:moldup_evet:783582088346468384> **${track.title}** isimli şarkı kuyruğa eklendi! [${message.author}]`)))
+player.on('queueEnd' , (message , queue) => message.channel.send(new Discord.MessageEmbed().setColor('BLUE').setDescription(`<:moldup_evet:783582088346468384> Kuyruktaki bütün şarkılar oynatıldığı için sesli kanaldan ayrılıyorum!`)))
+
 
 module.exports.player = player
