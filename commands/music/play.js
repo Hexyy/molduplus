d417101f34c686f472e83e5522525490142b1dbe
alputv <77716705+alputv@users.noreply.github.com>
2021-03-08 00:36:55 +0300

    Update play.js

--- commands/music/play.js
+++ commands/music/play.js
@@ -15,6 +15,6 @@ run: async (client , message ,args) => {
 if(!message.member.voice.channel) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setTitle('<:hata:813391295665930260> Hata').setDescription(`Bir sesli kanalda olmadan bu komutu kullanamazsın!`))  
 if(message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setTitle('<:hata:813391295665930260> Hata').setDescription(`Aynı sesli kanalda değiliz, biraz empati kur!`))  
 if(!args[0]) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setTitle('<:hata:813391295665930260> Hata').setDescription(`Lütfen bir şarkı adı yazınız!`))
-player.search(args.join(' '))
+await player.search(args.join(' '), "youtube")
 }
 }
