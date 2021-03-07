127325f2f8d86ec501c0c7de08b23c37327d6845
Hexyy <72667553+Hexyy@users.noreply.github.com>
2021-03-07 14:20:57 +0300

    Update kuyruk-döngüsü.js

--- commands/music/kuyruk-döngüsü.js
+++ commands/music/kuyruk-döngüsü.js
@@ -18,7 +18,7 @@ if(!client.player.isPlaying(message)) {
   return message.channel.send(new Discord.MessageEmbed().setTitle("<:hata:813391295665930260> Hata!").setColor("RED").setDescription(`Bu sunucuda her hangi bir şarkı çalmıyor!`))
   if(!client.player.getQueue(message)) {
   return message.channel.send(new Discord.MessageEmbed().setTitle("<:hata:813391295665930260> Hata!").setColor("RED").setDescription(`Bu sunucuda şarkı kuyruğu bulunmuyor!`))
-  }
+  }}
   if (!args[0]) return message.channel.send(new Discord.MessageEmbed().setTitle("<:hata:813391295665930260> Hata!").setColor("RED").setDescription(`Lütfen geçerli bir argüman girin!\n\n\`?şarkı-döngüsü aç\n?şarkı-döngüsü kapat\`  `))
 if (args[0] === "aç" || args[0] === "aktif") {
   client.player.setLoopMode(message, true)
