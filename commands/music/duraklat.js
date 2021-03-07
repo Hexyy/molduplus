3c046bb78799230736b26195180e15e268ba2a84
alputv <77716705+alputv@users.noreply.github.com>
2021-03-07 14:48:38 +0300

    Create duraklat.js

---
+++ commands/music/duraklat.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const { player } = require('../../settings/müzik')
+module.exports = {
+ name: 'duraklat',
+ aliases: ['durdur' , 'pause', 'dur', 'p'] ,
+ description: 'Çalan şarkıyı duraklatırsınız.',
+ usage: ['duraklat'],
+/** 
+* @param {Discord.Client} client
+* @param {Discord.Message} message
+* @param {String[]} args
+*/
+run: async (client , message ,args) => {
+if(!message.guild.me.hasPermission('SPEAK') && !message.guild.me.hasPermission('CONNECT')) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setTitle('<:hata:813391295665930260> Hata').setDescription(`O sesli kanalda konuşamıyorum, izinlerimi kontrol edin!`)) 
+if(!message.member.voice.channel) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setTitle('<:hata:813391295665930260> Hata').setDescription(`Bir sesli kanalda olmadan bu komutu kullanamazsın!`))  
+if(message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setTitle('<:hata:813391295665930260> Hata').setDescription(`Aynı sesli kanalda değiliz, biraz empati kur!`))
+if(!client.player.isPlaying(message)) {
+  return message.channel.send(new Discord.MessageEmbed().setTitle("<:hata:813391295665930260> Hata!").setColor("RED").setDescription(`Bu sunucuda her hangi bir şarkı çalmıyor!`))
+} else {
+client.player.pause(message) 
+message.react('⏸️')
+}
+}
+}
