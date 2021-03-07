d224bde3c873a6927dedb1fb5ad029451b36a6b9
alputv <77716705+alputv@users.noreply.github.com>
2021-03-08 00:24:19 +0300

    Update müzik.js

--- settings/müzik.js
+++ settings/müzik.js
@@ -5,8 +5,8 @@ autoSelfDeaf: true,
 })
 player.on('trackStart', (message, track) => message.channel.send(new Discord.MessageEmbed()
 .setColor('GREEN').setTitle(track.title).setURL(track.url).setDescription(`<:moldup_evet:783582088346468384> **${track.title}** isimli şarkı oynatılıyor! \`[${message.author.tag}]\``)))
-player.on('channelEmpty', (message, queue) => message.channel.send(new Discord.MessageEmbed().setColor('BLUE').setDescription('<:moldup_uyku:783582251836637214> Müzik çalınan kanalda kimse kalmadığı için şarkı durduruldu.'))
-player.on('botDisconnect', (message) => message.channel.send(new Discord.MessageEmbed().setColor('BLUE').setDescription('<:moldup_uyku:783582251836637214> Müzik çalınan kanaldan beni çıkarttığınız için şarkı durduruldu.'))
+player.on('channelEmpty', (message, queue) => message.channel.send(new Discord.MessageEmbed().setColor('BLUE').setDescription('<:moldup_uyku:783582251836637214> Müzik çalınan kanalda kimse kalmadığı için şarkı durduruldu.')))
+player.on('botDisconnect', (message) => message.channel.send(new Discord.MessageEmbed().setColor('BLUE').setDescription('<:moldup_uyku:783582251836637214> Müzik çalınan kanaldan beni çıkarttığınız için şarkı durduruldu.')))
 player.on('trackAdd', (message, queue, track) => message.channel.send(new Discord.MessageEmbed().setColor('BLUE').setTitle(track.title).setURL(track.url).setDescription(`<:moldup_evet:783582088346468384> **${track.title}** isimli şarkı kuyruğa eklendi! \`[${message.author.tag}]\``)))
 player.on('queueEnd' , (message , queue) => message.channel.send(new Discord.MessageEmbed().setColor('BLUE').setDescription(`<:moldup_uyku:783582251836637214> Kuyruktaki bütün şarkılar oynatıldığı için sesli kanaldan ayrılıyorum!`)))
 player.on('searchResults', (message, query, tracks) => {
@@ -29,8 +29,8 @@ player.on('searchInvalidResponse', (message, query, tracks, content, collector) 
     message.channel.send(new Discord.MessageEmbed().setTitle("<:hata:813391295665930260> Hata!").setColor("RED").setDescription(`1 ile ${tracks.length} arasında geçerli bir sayı yazmadınız, şarkı oynatmak için komutu tekrar yazmalısınız!`)
 
 })
-player.on('searchCancel', (message, query, tracks) => message.channel.send(new Discord.MessageEmbed().setTitle("<:hata:813391295665930260> Hata!").setColor("RED").setDescription('Şarkı numarasını zamanında belirtmediniz, şarkı oynatmak için komutu tekrar yazmalısınız!'))
-player.on('noResults', (message, query) => message.channel.send(new Discord.MessageEmbed().setTitle("<:hata:813391295665930260> Hata!").setColor("RED").setDescription(`\`${query}\` adında bir şarkı bulamadım.`))
+player.on('searchCancel', (message, query, tracks) => message.channel.send(new Discord.MessageEmbed().setTitle("<:hata:813391295665930260> Hata!").setColor("RED").setDescription('Şarkı numarasını zamanında belirtmediniz, şarkı oynatmak için komutu tekrar yazmalısınız!')))
+player.on('noResults', (message, query) => message.channel.send(new Discord.MessageEmbed().setTitle("<:hata:813391295665930260> Hata!").setColor("RED").setDescription(`\`${query}\` adında bir şarkı bulamadım.`)))
 player.on('error', (error, message) => {
     switch(error){
         case 'UnableToJoin':
