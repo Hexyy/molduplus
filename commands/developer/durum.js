705b472180d5c64066c1f913d6912d853ddcc554
alputv <77716705+alputv@users.noreply.github.com>
2021-03-08 10:42:04 +0300

    Create durum.js

---
+++ commands/developer/durum.js
@@ -0,0 +1,24 @@
+module.exports = {
+    name: 'durum',
+    aliases: ['status', 'durumdegis', 'durumdeğiş', 'durum-değiş', 'durum-degis'] ,
+   /** 
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+   run: async (client , message ,args) => {
+           if (message.author.id !== "696373721992003604" && message.author.id !== "722186767704522812") return
+           
+           let status = args[0]
+           let durum = args.slice(1).join(" ")
+           
+           
+           if (!status) return message.channel.send("Lütfen botun ayarlanacağı modu giriniz.")
+           if (!durum) return message.channel.send("Lütfen botun ayarlanacağı durumu giriniz.")
+           
+           let statü = status.replace("oynuyor", "PLAYING").replace("izliyor", "WATCHING").replace("dinliyor", "LISTENING")
+           client.user.setActivity(durum, { type: statü })
+           
+           message.channel.send("👍")
+}
+           }
