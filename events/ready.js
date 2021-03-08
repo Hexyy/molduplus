ff28d23e193c77f8e05aa7218e5ec1bde42fa9f0
alputv <77716705+alputv@users.noreply.github.com>
2021-03-08 10:52:11 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,14 +4,14 @@ const { Client } = require('discord.js')
  * @param {Client} client
  */
 module.exports = (client) => {
-const durumlar = ['bot.moldup.tk | ?yardım', 'youtube.com/AlpuTV | Yapımcımın kanalına abone olursan sevinirim 😊', 'BUGÜN HEXY\'NİN DOĞUM GÜNÜ!!! | Doğum Günün Kutlu Olsun 🎂']
+//const durumlar = ['bot.moldup.tk | ?yardım']
 
-setInterval(function() {
+//setInterval(function() {
 
-         var random = Math.floor(Math.random()*(durumlar.length-0+1)+0);
-         client.user.setActivity(durumlar[random], {"type": "WATCHING"});
+         //var random = Math.floor(Math.random()*(durumlar.length-0+1)+0);
+         client.user.setActivity('bot.moldup.tk | ?yardım', {"type": "WATCHING"});
 
-        }, 2 * 5000);
+        //}, 2 * 5000);
     
 console.log(require('chalk').blue(`${client.user.tag} ismiyle giriş yaptım!`));
 }
