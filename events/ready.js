54ee1de88c8ba81dbd61b1309662f5ec8a794310
alputv <77716705+alputv@users.noreply.github.com>
2021-03-08 11:01:52 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,14 +4,14 @@ const { Client } = require('discord.js')
  * @param {Client} client
  */
 module.exports = (client) => {
-//const durumlar = ['bot.moldup.tk | ?yardım']
+const durumlar = ['bot.moldup.tk | ?yardım']
 
-//setInterval(function() {
+setInterval(function() {
 
-         //var random = Math.floor(Math.random()*(durumlar.length-0+1)+0);
+         var random = Math.floor(Math.random()*(durumlar.length-0+1)+0);
          client.user.setActivity('bot.moldup.tk | ?yardım', {"type": "WATCHING"});
 
-        //}, 2 * 5000);
+        }, 2 * 5000);
     
 console.log(require('chalk').blue(`${client.user.tag} ismiyle giriş yaptım!`));
 }
