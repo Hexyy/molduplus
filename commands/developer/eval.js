46b0ed32a101be5536615e66ec728cba021f807c
alputv <77716705+alputv@users.noreply.github.com>
2021-03-09 12:06:30 +0300

    Update eval.js

--- commands/developer/eval.js
+++ commands/developer/eval.js
@@ -7,8 +7,13 @@ module.exports = {
    * @param {String[]} args
    */
    run: async (client , message ,args) => {
-           if (message.author.id !== "696373721992003604" && message.author.id !== "722186767704522812") return
-  try {
+    const noperm = new Discord.MessageEmbed()
+    .setColor('RED')
+    .setDescription(`**${message.author.username}** bu komutu kullanabilmek için \`botun yapımcısı\` olmalısın!`)
+    if(!message.guild.me.hasPermission(['SEND_MESSAGES','MANAGE_EMOJIS','EMBED_LINKS'])) return
+    if (message.author.id !== "696373721992003604" && message.author.id !== "722186767704522812") return message.channel.send(noperm.setTitle('<:hata:813391295665930260> Yetersiz Yetki!'))
+       
+       try {
       const code = args.join(" ");
 
       if(!code) return message.channel.send("Lütfen denenecek bir kod yazınız.")
