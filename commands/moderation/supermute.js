1746c8cb6fe86aef876e73377e91dd33b95f5bf0
alputv <77716705+alputv@users.noreply.github.com>
2021-03-15 13:21:11 +0300

    Update supermute.js

--- commands/moderation/supermute.js
+++ commands/moderation/supermute.js
@@ -43,12 +43,12 @@ if (!mutekisi) return message.channel.send(new Discord.MessageEmbed().setTitle("
   }
 
   let reason = args.slice(message.mentions.members.size).join(" ")
-    
-  let tagm = message.mentions.members.map(user => message.guild.members.cache.get(user.id).user.tag).join(", ")
-                                          
+                                             
   userlar.forEach(async u => await u.roles.add(muterol.id, `[ ${message.author.tag}: Süper Mute ] ${reason}`).catch(err => message.channel.send(`\`\`\`Bir hata oluştu: ${err}\`\`\``)))
   
-  return message.channel.send("**" + tagm + "** başarıyla süper mutelendi.")
+let tagm = message.mentions.members.map(user => message.guild.members.cache.get(user.id)).join(", ")
+   
+  return message.channel.send("<:verified:803327894444441640> **" + tagm + "** süper susturuldu.")
   
   }
   }
