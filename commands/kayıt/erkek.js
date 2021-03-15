b1b36d0720bd609208ec7045c1443ef1dae59747
alputv <77716705+alputv@users.noreply.github.com>
2021-03-15 10:49:11 +0300

    Update erkek.js

--- commands/kayıt/erkek.js
+++ commands/kayıt/erkek.js
@@ -28,15 +28,15 @@ let erkek = message.guild.roles.cache.get(erkekroleID);
 let yetkili = message.guild.roles.cache.get(yetkiliroleID);
 let kayıtsız = message.guild.roles.cache.get(kayıtsızroleID);
   
-      if (!erkek) return message.channel.send(nn.setColor('RED').setDescription(`<:moldup_hayir:783582180113907742> Kayıt sistemi ayarlamaları yapılmamış veya tamamlanmamış!`))
+      if (!erkek) return message.channel.send(new Discord.MessageEmbed().setTitle("<:hata:813391295665930260> Hata!").setColor("RED").setDescription(`**${message.author.username}** Kayıt sistemi ayarlamaları yapılmamış veya tamamlanmamış!`))
 
 
   if(!message.member.roles.cache.has(yetkiliroleID) && !message.member.hasPermission('MANAGE_ROLES')) {
     
     if (yetkili) {
-      return message.channel.send(nn.setColor('RED').setDescription(`<:moldup_sinirli:783582342643056661> ${yetkili} rolüne sahip olman gerekiyor.`));
+      return message.channel.send(new Discord.MessageEmbed().setTitle("<:hata:813391295665930260> Hata!").setColor("RED").setDescription(`**${message.author.username}** ${yetkili} rolüne sahip olman gerekiyor.`));
     } else {
-        return message.channel.send(new Discord.MessageEmbed().setTitle('Hata').setColor("RED").setDescription(`<:moldup_sinirli:783582342643056661> Bu komutu kullanmak için \`Rolleri Yönet\` iznine sahip olman gerekli.`))
+        return message.channel.send(new Discord.MessageEmbed().setTitle("<:hata:813391295665930260> Hata!").setColor("RED").setDescription(`**${message.author.username}** Bu komutu kullanmak için \`Rolleri Yönet\` iznine sahip olman gerekli.`))
     }
   } 
 
@@ -48,7 +48,7 @@ isim = args.slice(1).join(' ');
 isim = member.user.username;
 }
 
-if(!args[0] || !message.mentions.members.first()) return message.channel.send(nn.setColor('RED').setDescription(`<:moldup_hayir:783582180113907742> Bir kullanıcı etiketleyerek tekrar deneyiniz.`)).then(a => a.delete({timeout: 10000}));
+if(!args[0] || !message.mentions.members.first()) return message.channel.send(new Discord.MessageEmbed().setTitle("<:hata:813391295665930260> Bir Kullanıcı Etiketlemen Gerekiyor!").setColor("RED").setDescription(`**${message.author.username}** Bir kullanıcı etiketleyerek tekrar deneyiniz.`))
 const n = await kayit.fetch(`tag.${message.guild.id}`);
 
   
