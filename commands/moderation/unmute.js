aac79c173f1ca365722142dee674afe4c8f2b0f5
alputv <77716705+alputv@users.noreply.github.com>
2021-03-15 13:42:06 +0300

    Create unmute.js

--- commands/moderation/unmute.js
+++ commands/moderation/unmute.js
@@ -35,7 +35,7 @@ if (!mutekisi) return message.channel.send(new Discord.MessageEmbed().setTitle("
   
  let tagm = message.mentions.members.map(user => message.guild.members.cache.get(user.id)).join(", ")
    
-  return message.channel.send("<:verified:803327894444441640> " + tagm + " susturulmaları kaldırıldı.")
+  return message.channel.send("<:verified:803327894444441640> " + tagm + " üyelerinin susturulmaları kaldırıldı.")
 
 }
 }
