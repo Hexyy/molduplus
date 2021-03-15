d93369cbdcf7bd09e16d59781eb3f621e58208c1
alputv <77716705+alputv@users.noreply.github.com>
2021-03-15 10:46:12 +0300

    Update erkek-sil.js

--- commands/kayıt/erkek-sil.js
+++ commands/kayıt/erkek-sil.js
@@ -18,7 +18,7 @@ run: async (client , message ,args) => {
 
 exports.run = async (client, message, args) => {
   const nn = new Discord.MessageEmbed().setThumbnail();
-  if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(new Discord.MessageEmbed().setTitle("<:hata:813391295665930260> Hata!").setColor("RED").setDescription(`Bu komutu kullanmak için \`Yönetici\` iznine sahip olman gerekli.`))
+  if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(new Discord.MessageEmbed().setTitle("<:hata:813391295665930260> Yetersiz Yetki!").setColor("RED").setDescription(`**${message.author.username}** Bu komutu kullanmak için \`Yönetici\` iznine sahip olman gerekli.`))
 
 await kayit.delete(`erkek.${message.guild.id}`);
 message.channel.send(nn.setTitle(`Görev Tamamlandı!`).setColor("#22BF41").setDescription(`${tik} Kayıt sistemi erkek rolü başarıyla silindi.`))
