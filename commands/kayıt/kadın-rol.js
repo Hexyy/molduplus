07e98a3d205b4b28637e341f1bd49931ecd14ddc
alputv <77716705+alputv@users.noreply.github.com>
2021-03-15 11:14:30 +0300

    Update kadın-rol.js

--- commands/kayıt/kadın-rol.js
+++ commands/kayıt/kadın-rol.js
@@ -20,9 +20,9 @@ run: async (client , message ,args) => {
 const nn = new Discord.MessageEmbed().setThumbnail();
   if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(new Discord.MessageEmbed().setTitle("<:hata:813391295665930260> Yetersiz Yetki!").setColor("RED").setDescription(`**${message.author.username}** Bu komutu kullanmak için \`Yönetici\` iznine sahip olman gerekli.`))
 let role = message.mentions.roles.first();
-if(!role) return message.channel.send(new Discord.MessageEmbed().setTitle("<:hata:813391295665930260> Hata!").setColor("RED").setDescription(`**${message.author.username}** Bir rol etiketlemeyi unuttunuz.`)).then(a => a.delete({timeout: 10000}));
+if(!role) return message.channel.send(new Discord.MessageEmbed().setTitle("<:hata:813391295665930260> Hata!").setColor("RED").setDescription(`**${message.author.username}** Bir rol etiketlemeyi unuttunuz.`))
 kayit.set(`kadın.${message.guild.id}`, role.id);
-message.channel.send(nn.setTitle(`Görev Tamamlandı!`).setColor("#22BF41").setDescription(`${tik} Kayıt sistemi kadın rolünü ${role} rol olarak ayarladım.`).setColor(AloneDogru)).then(a => a.delete({timeout: 10000}));
+message.channel.send(nn.setTitle(`Görev Tamamlandı!`).setColor("#22BF41").setDescription(`${tik} Kayıt sistemi kadın rolünü ${role} rol olarak ayarladım.`).setColor(AloneDogru))
 
 }
 }
