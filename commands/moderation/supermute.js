1876d0e0eedec75956ef20d5de81e8d38b10bf53
alputv <77716705+alputv@users.noreply.github.com>
2021-03-16 11:58:23 +0300

    Update supermute.js

--- commands/moderation/supermute.js
+++ commands/moderation/supermute.js
@@ -54,7 +54,7 @@ await mod.add(`işlem_${message.guild.id}`, 1)
  
  let işlem = await mod.fetch(`işlem_${message.guild.id}`)
  
-  return message.channel.send("<:verified:803327894444441640> `İşlem #" + işlem + "` " + tagm + " süper susturuldu.")
+  return message.channel.send("<:tik:766402113185120336> `İşlem #" + işlem + "` " + tagm + " süper susturuldu.")
   
   }
   }
