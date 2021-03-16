3d83f84e8245df15f41c83f2c65ddcaf02f5b4bd
alputv <77716705+alputv@users.noreply.github.com>
2021-03-16 12:06:39 +0300

    Update channel-info.js

--- commands/info/channel-info.js
+++ commands/info/channel-info.js
@@ -20,10 +20,10 @@ let created = (await now) - kanal.createdTimestamp
 
 const embed = new Discord.MessageEmbed()
 .setColor('BLUE')
-.addField('• Kanal Adı' , kanal.name)
-.addField('• Kanal ID', kanal.id)
-.addField('• Son Mesaj' , `[${kanal.lastMessage.content}](${kanal.lastMessage.url})`)
-.addField('• Oluşturulma Tarihi' , moment.duration(created).format('Y [yıl], D [gün], H [saat], m [dakika],s [saniye]')+'önce')
+.addField('<:nokta:821307638473293834> Kanal Adı' , kanal.name)
+.addField('<:nokta:821307638473293834> Kanal ID', kanal.id)
+.addField('<:nokta:821307638473293834> Son Mesaj' , `[${kanal.lastMessage.content}](${kanal.lastMessage.url})`)
+.addField('<:nokta:821307638473293834> Oluşturulma Tarihi' , moment.duration(created).format('Y [yıl], D [gün], H [saat], m [dakika],s [saniye]')+' önce')
 message.channel.send(embed)
 }
 }
