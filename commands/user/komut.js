f6c1a0fd192410550c6db89f377e4253f9d6125a
alputv <77716705+alputv@users.noreply.github.com>
2021-03-17 09:28:04 +0300

    Update komut.js

--- commands/user/komut.js
+++ commands/user/komut.js
@@ -12,8 +12,10 @@ module.exports = {
 */
 run: async (client , message ,args) => {
 let cmd = client.commands.get(args[0]) || client.commands.get(client.aliases.get(args[0]))
-if(!args[0]) return message.channel.send(`Bir komut adı girmeyi unuttun!`)
-if(!cmd) return message.channel.send(`Komutlarımda **${args[0]}** adında komut bulamadım!`) 
+if(!args[0]) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setTitle('<:hata:813391295665930260> Bir Komut Adı Girmeyi Unuttun!')
+.setDescription(`**${message.author.username}** bir komut adı girmeyi unuttun!`))
+if(!cmd) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setTitle('<:hata:813391295665930260> Hata!')
+.setDescription(`Komutlarımda **${args[0]}** adında komut bulamadım!`)) 
 
 message.channel.send(`
 \`\`\`fix\n< ?${cmd.usage} >\`\`\`\`\`\`diff\n- Açıklama\n${cmd.description}\n\n- Diğer Kullanımlar\n${cmd.aliases.join(' , ')}\`\`\`\`\`\`md\n> [] = Gerekli argüman\n> {} = İsteğe bağlı argüman\`\`\``)
