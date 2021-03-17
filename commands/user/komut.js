f81cdbd48fccc2f4a3f1bb0184adc0d0e886efce
alputv <77716705+alputv@users.noreply.github.com>
2021-03-17 09:28:28 +0300

    Update komut.js

--- commands/user/komut.js
+++ commands/user/komut.js
@@ -15,7 +15,7 @@ let cmd = client.commands.get(args[0]) || client.commands.get(client.aliases.get
 if(!args[0]) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setTitle('<:hata:813391295665930260> Bir Komut Adı Girmeyi Unuttun!')
 .setDescription(`**${message.author.username}** bir komut adı girmeyi unuttun!`))
 if(!cmd) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setTitle('<:hata:813391295665930260> Hata!')
-.setDescription(`Komutlarımda **${args[0]}** adında komut bulamadım!`)) 
+.setDescription(`Komutlarımda **\`${args[0]}\`** adında komut bulamadım!`)) 
 
 message.channel.send(`
 \`\`\`fix\n< ?${cmd.usage} >\`\`\`\`\`\`diff\n- Açıklama\n${cmd.description}\n\n- Diğer Kullanımlar\n${cmd.aliases.join(' , ')}\`\`\`\`\`\`md\n> [] = Gerekli argüman\n> {} = İsteğe bağlı argüman\`\`\``)
