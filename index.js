7d975ffba241845ac9b103335c694ebedb6d9771
alputv <77716705+alputv@users.noreply.github.com>
2021-03-20 20:43:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const cekilis = new giveaway.GiveawaysManager(client, {
         exemptPermissions: [],
         embedColor: '#00FFC3',
         embedColorEnd: '#353940',
-        reaction: '🎉'
+        reaction: '822029668294721566'
     }  
 })
 client.çekiliş = cekilis
