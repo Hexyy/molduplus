042ae5a3a8cfff7abb45b9feb86d9c41a300d2cb
Hexyy <72667553+Hexyy@users.noreply.github.com>
2022-08-08 16:10:46 +0300

    Update ayarlar.js

--- settings/ayarlar.js
+++ settings/ayarlar.js
@@ -1,5 +1,5 @@
 const ayarlar = {
-token: 'NzYxNTcxNDQzMTk2NTU5Mzcx.X3cirQ.gF5LyA8_ITuvpHPlwsiH1ljGZiA',
+token: '',
 prefix: '?',
 mongoDB:'mongodb+srv://moldup:moldup123@moldup.ulpqw.mongodb.net/moldup?retryWrites=true&w=majority'
 }
